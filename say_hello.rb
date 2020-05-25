eb6e1f2db995e60bdd9dcca6b751b22c7841d389
Phill McGee <entrenest@outlook.com>
2020-05-25 19:22:50 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -8,4 +8,8 @@ say_hello("Gabriela")
 # I call on the method, say_hello, and give it the string "Gabriela" 
 
 # The method prints this text to the screen:
+<<<<<<< HEAD
 #Hello Gabriela!
+=======
+Hello Gabriela!
+>>>>>>> ddc34a521809b1c5d695d575d1281e9a665cf69c
